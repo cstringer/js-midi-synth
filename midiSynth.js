081b9a829151689bc40c949c6f576eb754bac33b
Chris Stringer <cstringer42@gmail.com>
2020-01-02 18:54:00 -0600

    Convert to ES6 class, related changery

--- midiSynth.js
+++ midiSynth.js
@@ -1,224 +1,230 @@
-// DOM refs
-let messages, startBtn, status;
+(function() {
+	class MidiSynth {
+		constructor() {
+			// DOM refs
+			this.messages = null;
+			this.startBtn = null;
+			this.status = null;
 
-// WebAudio refs
-let audioContext, masterGainNode, masterFilter;
+			// WebAudio refs
+			this.audioContext = null;
+			this.masterGainNode = null;
+			this.masterFilter = null;
 
-// connected MIDI devices, keyed by name
-const midiPorts = {};
+			// connected MIDI devices, keyed by name
+			this.midiPorts = {};
 
-// currently-pressed keys, by MIDI note #
-const pressedKeys = {};
+			// currently-pressed keys, by MIDI note #
+			this.pressedKeys = {};
 
-// wait for it...
-document.addEventListener('DOMContentLoaded', onDomContentLoaded);
+			this.onMidiMessage = this.onMidiMessage.bind(this);
+		}
+
+		start() {
+			this.messages = document.getElementById('messages');
+			this.startBtn = document.getElementById('start');
+			this.status = document.getElementById('status');
+
+			if (!navigator.requestMIDIAccess) {
+				this.logStatus('No MIDI support available.');
+				this.disableStartBtn();
+				return;
+			}
+
+			this.setupMidi();
+
+			this.startBtn.addEventListener('click', this.onStartBtnClick.bind(this));
+		}
+
+		onStartBtnClick() {
+			this.createAudio();
+			this.disableStartBtn({ btnText: 'Synth Running' });
+		}
+
+		/*===== MIDI =====*/
+
+		async setupMidi() {
+			this.logStatus('Setting up MIDI...');
+
+			try {
+				const access = await navigator.requestMIDIAccess();
+				access.addEventListener('statechange', this.onStateChange.bind(this));
+				access.inputs.forEach(this.enableMidiInput.bind(this));
+			} catch(e) {
+				this.logStatus('Error setting up MIDI: ' + e.message);
+			}
+		}
+
+		onStateChange({ port }) {
+			this.logStatus(`State change: ${port.name}, ${port.state}`);
+
+			if (port.type === 'input') {
+				switch (port.state) {
+					case 'connected':
+						this.enableMidiInput(port);
+						break;
+
+					case 'disconnected':
+						this.disableMidiInput(port);
+						break;
+				}
+			}
+		}
+
+		enableMidiInput(input = {}) {
+			if (this.midiPorts[input.name]) { return; }
+
+			this.logStatus(`Enabling MIDI input: '${input.name}'`);
+			input.addEventListener('midimessage', this.onMidiMessage);
+
+			this.midiPorts[input.name] = input;
+		}
+
+		disableMidiInput(input = {}) {
+			if (!this.midiPorts[input.name]) { return; }
+
+			this.logStatus(`Disabling MIDI input: '${input.name}'`);
+			input.removeEventListener('midimessage', this.onMidiMessage);
+
+			delete this.midiPorts[input.name];
+		}
+
+		onMidiMessage(message = {}) {
+			this.printMessageInfo(message);
+
+			const [statusByte, data1, data2] = message.data;
+
+			if (this.isNoteOn(statusByte)) {
+				this.playNote(data1, data2);
+
+			} else if (this.isNoteOff(statusByte)) {
+				this.stopNote(data1);
+
+			// https://www.midi.org/specifications-old/item/table-3-control-change-messages-data-bytes-2
+			} else if (statusByte === 176) {
+				switch(data1) {
+						// modulation
+					case 1:
+						break;
+
+						// volume
+					case 7:
+						this.setMasterGain(data2 / 127);
+						break;
+				}
+
+			} else if (statusByte === 224) {
+				// pitch bend
+				this.setMasterFilterFreq(data2);
+			}
+		}
+
+		isNoteOn(statusByte) {
+			return (statusByte >= 144 && statusByte <= 159);
+		}
+
+		isNoteOff(statusByte) {
+			return (statusByte >= 128 && statusByte <= 143);
+		}
+
+		// https://en.wikipedia.org/wiki/MIDI_tuning_standard#Frequency_values
+		midiNoteToFreq(note) {
+			return Math.pow(2, (note - 69) / 12) * 440;
+		}
+
+		midiVelocityToGain(velocity) {
+			return Math.log(Math.pow(10, velocity)) / 292.4283068102438;
+		}
 
 
-function onDomContentLoaded() {
-  messages = document.getElementById('messages');
-  startBtn = document.getElementById('start');
-  status = document.getElementById('status');
+		/*===== WebAudio =====*/
 
-  if (!navigator.requestMIDIAccess) {
-    logStatus('No MIDI support available.');
-    disableStartBtn();
-    return;
-  }
+		createAudio() {
+			this.logStatus('Starting AudioContext...');
 
-  setupMidi();
+			const ACtx = window.AudioContext || window.webkitAudioContext;
+			if (!ACtx) {
+				this.logStatus('No constructor for AudioContext found.');
+				return;
+			}
 
-  startBtn.addEventListener('click', onStartBtnClick);
-}
+			this.audioContext = new ACtx();
 
-function onStartBtnClick() {
-  createAudio();
-  disableStartBtn({ btnText: 'Synth Running' });
-}
+			this.masterGainNode = this.audioContext.createGain();
+			this.masterGainNode.gain.value = 0.5;
 
-/*===== MIDI =====*/
+			this.masterFilter = this.createFilter();
 
-async function setupMidi() {
-  logStatus('Setting up MIDI...');
+			this.masterGainNode.connect(this.masterFilter);
+			this.masterFilter.connect(this.audioContext.destination);
+		}
 
-  try {
-    const access = await navigator.requestMIDIAccess();
-    access.addEventListener('statechange', onStateChange);
-    access.inputs.forEach(enableMidiInput);
-  } catch(e) {
-    logStatus('Error setting up MIDI: ' + e.message);
-  }
-}
+		createFilter() {
+			const filter = this.audioContext.createBiquadFilter();
+			filter.type = 'lowpass';
+			filter.frequency.value = 1000;
+			filter.Q.value = 8;
+			return filter;
+		}
 
-function onStateChange({ port }) {
-  logStatus(`State change: ${port.name}, ${port.state}`);
+		playNote(note, velocity) {
+			if (!this.audioContext) { return; }
 
-  if (port.type === 'input') {
-    switch (port.state) {
-      case 'connected':
-        enableMidiInput(port);
-        break;
+			const freq = this.midiNoteToFreq(note);
+			const osc = this.createOscillator(freq);
 
-      case 'disconnected':
-        disableMidiInput(port);
-        break;
-    }
-  }
-}
+			const gainVal = this.midiVelocityToGain(velocity);
+			const gainNode = this.createGain(gainVal);
+			osc.connect(gainNode);
 
-function enableMidiInput(input = {}) {
-  if (midiPorts[input.name]) { return; }
+			this.pressedKeys[note] = osc;
+			osc.start();
+		}
 
-  logStatus(`Enabling MIDI input: '${input.name}'`);
-  input.addEventListener('midimessage', onMidiMessage);
+		stopNote(note) {
+			let osc = this.pressedKeys[note];
+			if (!osc) { return; }
 
-  midiPorts[input.name] = input;
-}
+			osc.stop();
+			delete this.pressedKeys[note];
+			osc = null;
+		}
 
-function disableMidiInput(input = {}) {
-  if (!midiPorts[input.name]) { return; }
+		createOscillator(frequency) {
+			const osc = this.audioContext.createOscillator();
+			osc.type = 'square';
+			osc.frequency.value = frequency;
+			return osc;
+		}
 
-  logStatus(`Disabling MIDI input: '${input.name}'`);
-  input.removeEventListener('midimessage', onMidiMessage);
+		createGain(value) {
+			const gainNode = this.audioContext.createGain();
+			gainNode.connect(this.masterGainNode);
+			gainNode.gain.value = value;
+			return gainNode;
+		}
 
-  delete midiPorts[input.name];
-}
+		setMasterGain(value) {
+			if (this.masterGainNode) {
+				this.masterGainNode.gain.value = value;
+			}
+		}
 
-function onMidiMessage(message = {}) {
-  printMessageInfo(message);
+		setMasterFilterFreq(data) {
+			if (this.masterFilter) {
+				this.masterFilter.frequency.value = (data / 64) * 1000;
+			}
+		}
 
-  const [statusByte, data1, data2] = message.data;
+		/*===== UI and such =====*/
 
-  if (isNoteOn(statusByte)) {
-    playNote(data1, data2);
+		printMessageInfo({ data, target }) {
+			let noteFreq = '';
+			if (this.isNoteOn(data[0]) || this.isNoteOff(data[0])) {
+				noteFreq = `(${this.midiNoteToFreq(data[1])})`;
+			}
 
-  } else if (isNoteOff(statusByte)) {
-    stopNote(data1);
-
-  // https://www.midi.org/specifications-old/item/table-3-control-change-messages-data-bytes-2
-  } else if (statusByte === 176) {
-    switch(data1) {
-      // modulation
-      case 1:
-        break;
-
-      // volume
-      case 7:
-        setMasterGain(data2 / 127);
-        break;
-    }
-
-  } else if (statusByte === 224) {
-    // pitch bend
-    setMasterFilterFreq(data2);
-  }
-}
-
-function isNoteOn(statusByte) {
-  return (statusByte >= 144 && statusByte <= 159);
-}
-
-function isNoteOff(statusByte) {
-  return (statusByte >= 128 && statusByte <= 143);
-}
-
-// https://en.wikipedia.org/wiki/MIDI_tuning_standard#Frequency_values
-function midiNoteToFreq(note) {
-  return Math.pow(2, (note - 69) / 12) * 440;
-}
-
-function midiVelocityToGain(velocity) {
-  return Math.log(Math.pow(10, velocity)) / 292.4283068102438;
-}
-
-
-/*===== WebAudio =====*/
-
-function createAudio() {
-  logStatus('Starting AudioContext...');
-
-  const ACtx = window.AudioContext || window.webkitAudioContext;
-  if (!ACtx) {
-    logStatus('No constructor for AudioContext found.');
-    return;
-  }
-
-  audioContext = new ACtx();
-
-  masterGainNode = audioContext.createGain();
-  masterGainNode.gain.value = 0.5;
-
-  masterFilter = createFilter();
-
-  masterGainNode.connect(masterFilter);
-  masterFilter.connect(audioContext.destination);
-}
-
-function createFilter() {
-  const filter = audioContext.createBiquadFilter();
-  filter.type = 'lowpass';
-  filter.frequency = 1000;
-  filter.Q = 8;
-  return filter;
-}
-
-function playNote(note, velocity) {
-  if (!audioContext) { return; }
-
-  const freq = midiNoteToFreq(note);
-  const osc = createOscillator(freq);
-
-  const gainVal = midiVelocityToGain(velocity);
-  const gainNode = createGain(gainVal);
-  osc.connect(gainNode);
-
-  pressedKeys[note] = osc;
-  osc.start();
-}
-
-function stopNote(note) {
-  let osc = pressedKeys[note];
-  if (!osc) { return; }
-
-  osc.stop();
-  delete pressedKeys[note];
-  osc = null;
-}
-
-function createOscillator(frequency) {
-  const osc = audioContext.createOscillator();
-  osc.type = 'square';
-  osc.frequency.value = frequency;
-  return osc;
-}
-
-function createGain(value) {
-  const gainNode = audioContext.createGain();
-  gainNode.connect(masterGainNode);
-  gainNode.gain.value = value;
-  return gainNode;
-}
-
-function setMasterGain(value) {
-  if (masterGainNode) {
-    masterGainNode.gain.value = value;
-  }
-}
-
-function setMasterFilterFreq(data) {
-  if (masterFilter) {
-    masterFilter.frequency.value = (data / 64) * 1000;
-  }
-}
-
-/*===== UI and such =====*/
-
-function printMessageInfo({ data, target }) {
-  let noteFreq = '';
-  if (isNoteOn(data[0]) || isNoteOff(data[0])) {
-    noteFreq = `(${midiNoteToFreq(data[1])})`;
-  }
-
-  messages.innerText = `
+			this.messages.innerText = `
 =====================
 ${target.name}
 ---------------------
@@ -226,19 +232,28 @@ Status: ${data[0]}
 Data 1: ${data[1]} ${noteFreq}
 Data 2: ${data[2]}
 =====================
-  `;
-}
+	`;
+		}
 
-function logStatus(message = '') {
-  /*eslint-disable no-console*/
-  console.log(message);
-  /*eslint-enable no-console*/
-  status.innerText += `${message}\n`;
-}
+		logStatus(message = '') {
+			/*eslint-disable no-console*/
+			console.log(message);
+			/*eslint-enable no-console*/
+			this.status.innerText += `${message}\n`;
+		}
 
-function disableStartBtn({ btnText }) {
-  startBtn.setAttribute('disabled', 'disabled');
-  if (btnText) {
-    startBtn.innerText = btnText;
-  }
-}
+		disableStartBtn({ btnText }) {
+			this.startBtn.setAttribute('disabled', 'disabled');
+			if (btnText) {
+				this.startBtn.innerText = btnText;
+			}
+		}
+	}
+
+	// wait for it...
+	document.addEventListener('DOMContentLoaded', () => {
+		const ms = new MidiSynth();
+		ms.start();
+	});
+
+})();
